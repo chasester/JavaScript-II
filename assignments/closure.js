4a9571b9274fbcf4db17614a6fd883434afa50a6
Chase Wex <clapingmanly@hotmail.com>
2019-04-02 19:56:30 -0400

    Fixed last  chalange in closure.js with help.

--- assignments/closure.js
+++ assignments/closure.js
@@ -14,7 +14,7 @@ var x = 0;
 // ==== Challenge 2: Create a counter function ====
 
 // Example usage: const newCounter = counter();
-var increment = (function(n) {
+var increment = (function(n) {  // completes llFE requirement
   return function() {
     n += 1;
     return n;
@@ -25,8 +25,21 @@ increment();//2
 console.log(increment());
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = {
+
+var counter = 0;
+const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
+
+  let decrement = () => counter--;
+  let increment = () => counter++;
+  let obj = { increment, decrement }
+  return obj;
+}
+
+
+let foo = counterFactory();
+
+console.log(foo.increment());
+console.log(foo.decrement());
