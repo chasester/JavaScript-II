7aedbc32ea715d3deaf267167aee9eff88d23019
Chase Wex <clapingmanly@hotmail.com>
2019-04-02 17:44:50 -0400

    finished closure to spec.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,20 +1,31 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+(function Kingdom()
+{
+  (() =>
+  {
+    console.log("Im in the city");
+  })();
+})();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
-
+var x = 0;
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
+
 // Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+var increment = (function(n) {
+  return function() {
+    n += 1;
+    return n;
+  }
+}(0));
+increment();//1
+increment();//2
+console.log(increment());
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
+const counterFactory = {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
